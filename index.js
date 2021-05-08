cc3806be2c7128886356d79367fb064913c93a7c
jlarrazabal <69408299+jlarrazabal@users.noreply.github.com>
2021-05-08 00:55:05 -0400

    Index.js code complete - finalizing functions to capture the user input and create the new HTML file.
    Index.js code complete - finalizing functions to capture the user input and create the new HTML file.

--- index.js
+++ index.js
@@ -5,241 +5,301 @@ const path = require("path");
 
 //Global Variables:
 const teamMembers = [];
+const manangerQuestions = [{
+    type: "input",
+    name: "teamManagerName",
+    message: "What is the name of the team's Manager?"
+  },
+  {
+    type: "input",
+    name: "employeeID",
+    message: "What is the employee ID number of the team's Manager?"
+  },
+  {
+    type: "input",
+    name: "employeeEmail",
+    message: "What is the email address of the team's Manager?"
+  },
+  {
+    type: "input",
+    name: "managerOfficeNumber",
+    message: "What is the Manager's office number?"
+  }
+];
 
-//Adding Classes
+const teamMembersQuestions = [{
+    type: "input",
+    name: "teamMemberName",
+    message: "What is the name of the team member that you want to add to the group?"
+  },
+  {
+    type: "input",
+    name: "employeeID",
+    message: "What is the employee ID number of the team member that you want to add?"
+  },
+  {
+    type: "input",
+    name: "employeeEmail",
+    message: "What is the new team member's email address?"
+  }
+];
+
+const engineerQuestions = [{
+  type: "input",
+  name: "engineerGitHubUsername",
+  message: "What is the GitHub username of the engineer that you want to add to the group?"
+}];
+
+const internQuestions = [{
+  type: "input",
+  name: "internSchool",
+  message: "To what school does the intern that you want to add to the group belongs to?"
+}];
+
+const addNewTeamMemberQuestion = [{
+  type: "list",
+  name: "addNewTeamMember",
+  message: "Do you want to add another team member?",
+  choices: ["YES", "NO"]
+}];
+
+const employeeTypeQuestion = [{
+  type: "list",
+  name: "employeeType",
+  message: "What type of employee do you want to add to the team",
+  choices: ["Engineer", "Intern"]
+}];
+
+const teamNameQuestion = [{
+  type: "input",
+  name: "teamName",
+  message: "What is the name of the team?"
+}];
+
+//Classes
 class Employee {
   constructor(name, id, email) {
     this.name = name;
     this.id = id;
     this.email = email;
-    this.getName = function() {
-      inquirer.prompt([{
-        type: "input",
-        name: "name",
-        message: "What is the name of the employee to be added to the group? (Note: the first Entry will be the manager of the team!)"
-      }]).then((answers) => {
-        if (answers.name === "") {
-          const err = "Name cannot be empty; please try again";
-          console.log(err);
-        } else {
-          this.name = answers.name;
-        }
-      });
-    };
-    this.getId = function() {
-      inquirer.prompt([{
-        type: "input",
-        name: "id",
-        message: "What is employee id number of the new team member?"
-      }]).then((answers) => {
-        if (answers.id === "" || isNaN(parseInt(answers.id))) {
-          const err = "Employee ID cannot be empty or the ID that was provided is not a number; please try again";
-          console.log(err);
-        } else {
-          this.id = answers.id;
-        }
-      });
-    };
-    this.getEmail = function() {
-      inquirer.prompt([{
-        type: "input",
-        name: "email",
-        message: "What is the email the new team member?"
-      }]).then((answers) => {
-        if (answers.email === "") {
-          const err = "Error: Email cannot be empty; please try again.";
-          console.log(err);
-        } else {
-          this.email = answers.email;
-        }
-      });
-    };
-    this.getRole= function() {
-      return "Employe";
-    }
   }
-}
+};
 
 class Manager extends Employee {
   constructor(name, id, email, managerOfficeNumber) {
     super(name, id, email);
-    this.getRole = function(){
-      return "Manager";
-    };
     this.managerOfficeNumber = managerOfficeNumber;
-    this.getOfficeNumber = function(){
-      inquirer.prompt([{
-        type: "input",
-        name: "managerOfficeNumber",
-        message: "What is the manager of the team office number?"
-      }]).then((answers) => {
-        if (answers.managerOfficeNumber === ""||isNaN(parseInt(answers.managerOfficeNumber))) {
-          const err = "Error: Email cannot be empty or the value provide is not a number; please try again.";
-          console.log(err);
-        } else {
-          this.managerOfficeNumber = answers.managerOfficeNumber;
-        }
-      });
-    }
+    this.role = "Manager";
   }
-}
+};
 
 class Engineer extends Employee {
   constructor(name, id, email, github) {
     super(name, id, email);
-    this.getRole = function(){
-      return "Engineer";
-    };
     this.github = github;
-    this.getGitHub = function(){
-      inquirer.prompt([{
-        type: "input",
-        name: "github",
-        message: "What is the GitHub username of the engineer?"
-      }]).then((answers) => {
-        if (answers.github === "") {
-          const err = "Error: github username cannot be empty; please try again.";
-          console.log(err);
-        } else {
-          this.github = answers.github;
-        }
-      });
-    }
+    this.role = "Engineer";
   }
-}
+};
 
 class Intern extends Employee {
   constructor(name, id, email, school) {
     super(name, id, email);
-    this.getRole = function() {
-      return "Intern";
-    };
     this.school = school;
-    this.getSchool = function() {
-      inquirer.prompt([{
-        type: "input",
-        name: "school",
-        message: "What is the where does the intern study?"
-      }]).then((answers) => {
-        if (answers.school === "") {
-          const err = "Error: school cannot be empty; please try again.";
-          console.log(err);
-        } else {
-          this.school = answers.school;
-        }
-      });
-    }
+    this.role = "Intern";
   }
-}
+};
 
 //Functions to Build the Team and save the information on the teamMembers array
-
 const addManager = function() {
-  const newManager = new Manager();
-  const rol = newManager.getRole();
-  const addName = async function(){
-    newManager.getName();
-  };
+  inquirer.prompt(manangerQuestions).then((answers) => {
+    if (answers.teamManagerName === "" || answers.employeeID === "" || answers.employeeEmail === "" || answers.managerOfficeNumber === "") {
+      console.log("The information provide is not complete. For your convenience, the questions will be presented again in 2 seconds.");
+      setTimeout(() => {
+        addManager();
+      }, 2000);
+    } else if (isNaN(parseInt(answers.employeeID)) || isNaN(parseInt(answers.managerOfficeNumber))) {
+      console.log("Employee ID and Office Number must be numbers, please try again. For your convenience, the questions will be presented again in 2 seconds.");
+      setTimeout(() => {
+        addManager();
+      }, 2000);
+    } else {
+      const newManager = new Manager(answers.teamManagerName, answers.employeeID, answers.employeeEmail, answers.managerOfficeNumber);
+      teamMembers.push(newManager);
+      addTeamMember();
+    }
+  });
+};
 
-  const addId = async function() {
-    await addName();
-    newManager.getId();
-  };
+const addTeamMember = function() {
+  inquirer.prompt(addNewTeamMemberQuestion).then((answers) => {
+    if (answers.addNewTeamMember === "YES") {
+      inquirer.prompt(employeeTypeQuestion).then((answers) => {
+        switch (answers.employeeType) {
+          case "Engineer":
+            const newEngineerQuestions = teamMembersQuestions.concat(engineerQuestions);
+            addEngineer(newEngineerQuestions);
+            break;
+          case "Intern":
+            const newInternQuestions = teamMembersQuestions.concat(internQuestions);
+            addIntern(newInternQuestions);
+        }
+      });
+    } else {
+      console.log(teamMembers);
+      createHTMLfile();
+    }
+  });
+};
 
-  const addEmail = async function() {
-    await addId();
-    newManager.getEmail();
-  };
-
-  const addOffice = async function() {
-    await addEmail();
-    newManager.getOfficeNumber();
-  };
-
-  const updateTeam = async function() {
-    await addOffice();
-    teamMembers.push(newManager);
-  };
-
-  const addTeamMember = async function() {
-    await updateTeam();
-    addNewTeamMember();
-  };
-
-  addName();
-  addId();
-  addEmail();
-  addOffice();
-  updateTeam();
-  addTeamMember();
-}
-
-const addNewTeamMember = function() {
-  inquirer.prompt([{
-      type: "list",
-      name: "employeeType",
-      message: "What type of employee do you want to add to the team?",
-      choices: ["Engineer", "Intern"]
-    }]).then((answers) => {
-      switch (answers.employeeType) {
-        case "Engineer":
+const addEngineer = function(newEngineerQuestions) {
+  inquirer.prompt(newEngineerQuestions).then((answers) => {
+    if (answers.teamMemberName === "" || answers.employeeID === "" || answers.employeeEmail === "" || answers.engineerGitHubUsername === "") {
+      console.log("The information provide is not complete. For your convenience, the questions will be presented again in 2 seconds.");
+      setTimeout(() => {
         addEngineer();
-        break;
-        case "Intern":
-        addIntern();
-        break;
-      }
+      }, 2000);
+    } else if (isNaN(parseInt(answers.employeeID))) {
+      console.log("Employee ID must be a number, please try again. For your convenience, the questions will be presented again in 2 seconds.");
+      setTimeout(() => {
+        addEngineer();
+      }, 2000);
+    } else {
+      const newEngineer = new Engineer(answers.teamMemberName, answers.employeeID, answers.employeeEmail, answers.engineerGitHubUsername);
+      teamMembers.push(newEngineer);
+      addTeamMember();
+    }
+  });
+};
+
+const addIntern = function(newInternQuestions) {
+  inquirer.prompt(newInternQuestions).then((answers) => {
+    if (answers.teamMemberName === "" || answers.employeeID === "" || answers.employeeEmail === "" || answers.internSchool === "") {
+      console.log("The information provide is not complete. For your convenience, the questions will be presented again in 2 seconds.");
+      setTimeout(() => {
+        addEngineer();
+      }, 2000);
+    } else if (isNaN(parseInt(answers.employeeID))) {
+      console.log("Employee ID must be a number, please try again. For your convenience, the questions will be presented again in 2 seconds.");
+      setTimeout(() => {
+        addEngineer();
+      }, 2000);
+    } else {
+      const newIntern = new Intern(answers.teamMemberName, answers.employeeID, answers.employeeEmail, answers.internSchool);
+      teamMembers.push(newIntern);
+      addTeamMember();
+    }
+  });
+};
+
+// Function to write the index.html file with the information provided by the user:
+const createHTMLfile = function() {
+  inquirer.prompt(teamNameQuestion).then((answers) => {
+    if (answers.teamName === "") {
+      console.log("Team name connot be empty, please try again. For your convenience, the questions will be presented again in 2 seconds.");
+      setTimeout(() => {
+        addEngineer();
+      }, 2000);
+    } else {
+      let teamName = answers.teamName.replace(/\s+/g, "");
+      let cards = [];
+      const addCards = function(teamMembers) {
+        for (let i = 0; i < teamMembers.length; i++) {
+          switch (teamMembers[i].role) {
+            case "Manager":
+              let manager = `
+              <div class="card text-dark bg-light mb-3" style="width: 22rem; height: 16rem; margin: 1rem;">
+                <div class="card-header bg-primary">
+                  <h3 style="color:white;">${teamMembers[i].name}</h3>
+                  <h3 style="color:white;">☕ Manager</h3>
+                </div>
+                <div class="card-body">
+                  <ul class="list-group list-group-flush">
+                    <li class="list-group-item border border-1 border-bottom-0 rounded-top">ID: ${teamMembers[i].id}</li>
+                    <li class="list-group-item border border-1 border-bottom-0">Email: ${teamMembers[i].email}</li>
+                    <li class="list-group-item border border-1 rounded-bottom">Office Number: ${teamMembers[i].managerOfficeNumber}</li>
+                  </ul>
+                </div>
+              </div>
+              `;
+              cards.push(manager);
+              break;
+            case "Engineer":
+              let engineer = `
+              <div class="card text-dark bg-light mb-3" style="width: 22rem; height: 16rem; margin: 1rem;">
+                <div class="card-header bg-primary">
+                  <h3 style="color:white;">${teamMembers[i].name}</h3>
+                  <h3 style="color:white;">👓 Engineer</h3>
+                </div>
+                <div class="card-body">
+                  <ul class="list-group list-group-flush">
+                    <li class="list-group-item border border-1 border-bottom-0 rounded-top">ID: ${teamMembers[i].id}</li>
+                    <li class="list-group-item border border-1 border-bottom-0">Email: ${teamMembers[i].email}</li>
+                    <li class="list-group-item border border-1 rounded-bottom">GitHub: <a href="https://github.com/${teamMembers[i].github}">${teamMembers[i].github}</a></li>
+                  </ul>
+                </div>
+              </div>
+              `;
+              cards.push(engineer);
+              break;
+            case "Intern":
+              let intern = `
+              <div class="card text-dark bg-light mb-3" style="width: 22rem; height: 16rem; margin: 1rem;">
+                <div class="card-header bg-primary">
+                  <h3 style="color:white;">${teamMembers[i].name}</h3>
+                  <h3 style="color:white;">👨‍🎓 Intern</h3>
+                </div>
+                <div class="card-body">
+                  <ul class="list-group list-group-flush">
+                    <li class="list-group-item border border-1 border-bottom-0 rounded-top">ID: ${teamMembers[i].id}</li>
+                    <li class="list-group-item border border-1 border-bottom-0">Email: ${teamMembers[i].email}</li>
+                    <li class="list-group-item border border-1 rounded-bottom">School: ${teamMembers[i].school}</li>
+                  </ul>
+                </div>
+              </div>
+              `;
+              cards.push(intern);
+          }
+        }
+      };
+      addCards(teamMembers);
+      let template = `
+      <!doctype html>
+      <html lang="en">
+      <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+        <style>
+        * {
+          margin: 0;
+          border: 0;
+          padding: 0;
+          font-family: 'Roboto', sans-serif;
+          font-size: 1rem;
+        }
+        </style>
+        <title>${answers.teamName} Team Profie</title>
+      </head>
+
+      <body class="vh-100 vw-100 bg-light d-flex justify-content-center flex-column">
+        <nav class="container-fluid d-flex justify-content-center bg-danger">
+          <h1 class="h1 lh-lg" style="color:white;">My Team</h1>
+        </nav>
+        <main class="h-100 w-75 bg-light d-flex justify-content-evenly align-items-flex-start flex-wrap flex-row" style="margin: 0 auto;">
+          ${cards.join("\n")}
+        </main>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+      </body>
+      </html>
+      `;
+      fs.writeFile(`${teamName}.html`, template, function(err) {
+        if (err) throw err;
+        console.log(`Your new file "${teamName}.html" has been created/updated successfully!`);
+      });
+    }
   });
 }
-
-// const addEngineer = function() {
-//   const newEngineer = new Engineer();
-//   let role = newEngineer.getRole();
-//   newEngineer.name = newEngineer.getName();
-//   newEngineer.id = newEngineer.getId();
-//   newEngineer.email = newEngineer.getEmail();
-//   newEngineer.github = newEngineer.getGitHub();
-//   teamMembers.push(newEngineer);
-//   inquirer.prompt([{
-//       type: "list",
-//       name: "addNewTeamMember",
-//       message: "do you want to add another team member?",
-//       choices: ["YES", "NO"]
-//     }]).then((answers) => {
-//       switch (answers.addNewTeamMember) {
-//         case "YES":
-//         addNewTeamMember();
-//         break;
-//         case "NO":
-//         console.log(teamMembers);
-//         break;
-//       }
-//   });
-// }
-//
-// const addIntern = function() {
-//   const newIntern = new Intern();
-//   let role = newIntern.getRole();
-//   newIntern.name = newIntern.getName();
-//   newIntern.id = newIntern.getId();
-//   newIntern.email = newIntern.getEmail();
-//   newIntern.school = newIntern.getSchool();
-//   teamMembers.push(newIntern);
-//   inquirer.prompt([{
-//       type: "list",
-//       name: "addNewTeamMember",
-//       message: "do you want to add another team member?",
-//       choices: ["YES", "NO"]
-//     }]).then((answers) => {
-//       switch (answers.addNewTeamMember) {
-//         case "YES":
-//         addNewTeamMember();
-//         break;
-//         case "NO":
-//         console.log(teamMembers);
-//         break;
-//       }
-//   });
-// }
 
 addManager();
